eadff23f9a3937944c55e71156c92a113d9de4af
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-05-18 10:22:43 -0400

    app create

---
+++ Sweetr/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Sweetr
+//
+//  Created by Whitney Lauren on 5/18/15.
+//  Copyright (c) 2015 Whitney Lauren. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
