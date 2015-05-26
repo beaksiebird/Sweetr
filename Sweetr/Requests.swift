addae9528a787894298cb0844fac51d27700d0a5
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-05-26 15:13:14 -0400

    Progress. Shows tweets

--- Sweetr/Requests.swift
+++ Sweetr/Requests.swift
@@ -16,6 +16,7 @@ class FourSquareRequest {
     let CLIENT_SECRET = "44G3OQYYGB0YUR1YP1NQHGOBFQZIZJJWWVMT4FNDLF1QDP1Q"
     let CLIENT_ID = "1KEKHZROTGNXKDAWRNGIQYYNY4JD3O1K2K334EH4LL0FGGJU"
     
+    
     func getVenuesWithLocation(location: CLLocation, completion: (venues: [AnyObject]) -> Bool) {
      
         //venues empty array
@@ -33,28 +34,128 @@ class FourSquareRequest {
 
 class TwitterRequest {
     
-    let API_URL = "“https://api.twitter.com/1.1/"
+    let API_URL = "https://api.twitter.com/1.1/"
+   
+    let API_KEY = "zhxFFFtwMBT9F8roBGwsZLThl"
+    let API_SECRET = "Vjo51dD8OJp4Anj5C9ImtBzNqGeV1fQXTbazrLxSpSu4LovwkP"
     
+    var API_ENCODED: String? {
+        
+        get {
+            
+            let str = API_KEY + ":" + API_SECRET
+            
+            let utf8str = str.dataUsingEncoding(NSUTF8StringEncoding)
+            
+            if let encoded = utf8str?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.allZeros){
+                
+                return "Basic " + encoded
+                
+            }
+            
+            return nil
+        }
+        
+    }
+
+    var TOKEN: String? {
+        
+        get {
+            
+            return NSUserDefaults.standardUserDefaults().objectForKey("twitterToken") as? String
+            
+        }
+        
+        set {
+         
+            NSUserDefaults.standardUserDefaults().setObject(newValue, forKey: "twitterToken")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            
+            
+        }
+    }
     
+    func getToken(completion: () -> ()) {
+        
+        let endpoint = "https://api.twitter.com/oauth2/token?grant_type=client_credentials"
+        
+        
+        if let url = NSURL(string: endpoint) {
+            
+            var request = NSMutableURLRequest(URL: url)
+            
+            request.HTTPMethod = "POST"
+            
+            request.setValue(API_ENCODED, forHTTPHeaderField: "Authorization")
+            request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
+            
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
+            
+                
+                var error: NSError? = nil
+                
+                if let returnInfo = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as? [String:AnyObject] {
+                    
+                    println(returnInfo)
+                    
+                    self.TOKEN = returnInfo["access_token"] as? String
+                    
+                    completion()
+                    
+                }
+                
+                if let error = error {
+                    println(error)
+            
+                }
+                
+            })
+            
+        }
+        
+        
+    }
     
     func findPopularFoodTweets(completion: (tweets: [AnyObject]) -> ()) {
         
-        let foundTweets: [AnyObject] = []
-        
+        if TOKEN == nil {
+            
+            getToken({ () -> () in
+                //do something
+                
+                self.findPopularFoodTweets(completion)
+                
+                })
+            
+            
+            return
+            
+        }
         
         let endpoint = API_URL + "search/tweets.json?q=Food&result_type=popular"
         
         if let url = NSURL(string: endpoint) {
             
-            let request = NSURLRequest(URL: url)
+            let request = NSMutableURLRequest(URL: url)
+            
+            request.setValue("Bearer " + TOKEN!, forHTTPHeaderField: "Authorization")
             
             NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
                 
                 var error: NSError? = nil
             
-                if let tweets = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as? [AnyObject] {
+                if let searchInfo = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as? [String:AnyObject] {
                    
-                    completion(tweets: tweets)
+                    println(searchInfo)
+
+                    if let tweets = searchInfo["statuses"] as? [AnyObject] {
+                        
+                        println(tweets)
+                     
+                        completion(tweets: tweets)
+                        
+                    }
                     
                 }
         
--- Sweetr/VenuesTableViewController.swift
+++ Sweetr/VenuesTableViewController.swift
@@ -10,113 +10,113 @@ import UIKit
 import CoreLocation
 
 class VenuesTableViewController: UITableViewController {
-    
-    var fakeVenues: [AnyObject?] = [
-        
-        [
-            "meta":["code":200],
-            "response":
-                ["venues":
-                    [
-                        ["id":"4c533f3930f92d7fc47074b8",
-                        "name":"Pizza Hut",
-                        "contact":[],
-                        "location":
-                            [
-                                "address":"59 Broad St",
-                                "lat":33.752965,
-                                "lng":-84.392394,
-                                "distance":17,
-                                "cc":"US",
-                                "city":"Atlanta",
-                                "state":"GA",
-                                "country":"United States",
-                                "formattedAddress":["59 Broad St","Atlanta, GA","United States"]],
-                                "categories":[["id":"4bf58dd8d48988d1ca941735",
-                                "name":"Pizza Place",
-                                "pluralName":"Pizza Places",
-                                "shortName":"Pizza",
-                                "icon":["prefix":"https:\\/\\/ss3.4sqi.net\\/img\\/categories_v2\\/food\\/pizza_","suffix":".png"],
-                                "primary":true]],
-                                "verified":false,
-                                "stats":["checkinsCount":1,
-                                "usersCount":1,
-                                "tipCount":0],
-                                "hasMenu":true,
-                                "menu":["type":"Menu","label":"Menu","anchor":
-                                "View Menu","url":"https:\\/\\/foursquare.com\\/v\\/pizza-hut\\/4c533f3930f92d7fc47074b8\\/menu","mobileUrl":"https:\\/\\/foursquare.com\\/v\\/4c533f3930f92d7fc47074b8\\/device_menu"],"specials":["count":0,"items":[]],"hereNow":["count":0,"summary":"Nobody here","groups":[]],"referralId":"v-1431978351"],
-                        [
-                        ["id":"50a53540e4b0d9744f1d22f2",
-                        "name":"5 POINTS CUSTOME STYLES",
-                        "contact":[],
-                        "location":
-                        [
-                            "lat":33.752716064453125,
-                            "lng":-84.39261627197266,
-                            "distance":20,
-                            "postalCode":"30303",
-                            "cc":"US",
-                            "city":"Atlanta",
-                            "state":"GA",
-                            "country":"United States",
-                            "formattedAddress":["Atlanta, GA 30303","United States"]],
-                            "categories":[["id":"4bf58dd8d48988d1ff941735",
-                            "name":"Miscellaneous Shop",
-                            "pluralName":"Miscellaneous Shops",
-                            "shortName":"Shop",
-                            "icon":["prefix":"https:\\/\\/ss3.4sqi.net\\/img\\/categories_v2\\/shops\\/default_","suffix":".png"],
-                            "primary":true]],
-                            "verified":false,
-                            "stats":["checkinsCount":5,
-                            "usersCount":4,"tipCount":0],
-                            "specials":["count":0,
-                            "items":[]],
-                            "hereNow":["count":0,
-                            "summary":"Nobody here",
-                            "groups":[]],
-                            "referralId":"v-1431978351"],
-                            
-                        [
-                        ["id":"5408c0bc498e56004dd0ee7b",
-                        "name":"China Breeze",
-                        "contact":[],
-                        "location":[
-                            "address":"73 Broad Street",
-                            "lat":33.752744,
-                            "lng":-84.392217,
-                            "distance":21,
-                            "cc":"US",
-                            "city":"Atlanta",
-                            "state":"GA",
-                            "country":"United States",
-                            "formattedAddress":["73 Broad Street","Atlanta, GA","United States"]],
-                            "categories":[["id":"4bf58dd8d48988d14e941735",
-                            "name":"American Restaurant",
-                            "pluralName":"American Restaurants",
-                            "shortName":"American",
-                            "icon":["prefix":"https:\\/\\/ss3.4sqi.net\\/img\\/categories_v2\\/food\\/default_",
-                            "suffix":".png"],
-                            "primary":true]],
-                            "verified":false,
-                            "stats":["checkinsCount":2,
-                            "usersCount":1,
-                            "tipCount":1],
-                            "specials":[
-                            "count":0,
-                            "items":[]],
-                            "hereNow":[
-                            "count":0,
-                            "summary":"Nobody here",
-                            "groups":[]],"referralId":"v-1431978351"],
-                            ["id":"5314b2dde4b0d805d3741323"
-                                
-                                ]
-                            ]
-                            ]
-                        ]
-                    ]
-            ]
-        ]
+//    
+//    var Venues: [AnyObject?] = [
+//        
+//        [
+//            "meta":["code":200],
+//            "response":
+//                ["venues":
+//                    [
+//                        ["id":"4c533f3930f92d7fc47074b8",
+//                        "name":"Pizza Hut",
+//                        "contact":[],
+//                        "location":
+//                            [
+//                                "address":"59 Broad St",
+//                                "lat":33.752965,
+//                                "lng":-84.392394,
+//                                "distance":17,
+//                                "cc":"US",
+//                                "city":"Atlanta",
+//                                "state":"GA",
+//                                "country":"United States",
+//                                "formattedAddress":["59 Broad St","Atlanta, GA","United States"]],
+//                                "categories":[["id":"4bf58dd8d48988d1ca941735",
+//                                "name":"Pizza Place",
+//                                "pluralName":"Pizza Places",
+//                                "shortName":"Pizza",
+//                                "icon":["prefix":"https:\\/\\/ss3.4sqi.net\\/img\\/categories_v2\\/food\\/pizza_","suffix":".png"],
+//                                "primary":true]],
+//                                "verified":false,
+//                                "stats":["checkinsCount":1,
+//                                "usersCount":1,
+//                                "tipCount":0],
+//                                "hasMenu":true,
+//                                "menu":["type":"Menu","label":"Menu","anchor":
+//                                "View Menu","url":"https:\\/\\/foursquare.com\\/v\\/pizza-hut\\/4c533f3930f92d7fc47074b8\\/menu","mobileUrl":"https:\\/\\/foursquare.com\\/v\\/4c533f3930f92d7fc47074b8\\/device_menu"],"specials":["count":0,"items":[]],"hereNow":["count":0,"summary":"Nobody here","groups":[]],"referralId":"v-1431978351"],
+//                        [
+//                        ["id":"50a53540e4b0d9744f1d22f2",
+//                        "name":"5 POINTS CUSTOME STYLES",
+//                        "contact":[],
+//                        "location":
+//                        [
+//                            "lat":33.752716064453125,
+//                            "lng":-84.39261627197266,
+//                            "distance":20,
+//                            "postalCode":"30303",
+//                            "cc":"US",
+//                            "city":"Atlanta",
+//                            "state":"GA",
+//                            "country":"United States",
+//                            "formattedAddress":["Atlanta, GA 30303","United States"]],
+//                            "categories":[["id":"4bf58dd8d48988d1ff941735",
+//                            "name":"Miscellaneous Shop",
+//                            "pluralName":"Miscellaneous Shops",
+//                            "shortName":"Shop",
+//                            "icon":["prefix":"https:\\/\\/ss3.4sqi.net\\/img\\/categories_v2\\/shops\\/default_","suffix":".png"],
+//                            "primary":true]],
+//                            "verified":false,
+//                            "stats":["checkinsCount":5,
+//                            "usersCount":4,"tipCount":0],
+//                            "specials":["count":0,
+//                            "items":[]],
+//                            "hereNow":["count":0,
+//                            "summary":"Nobody here",
+//                            "groups":[]],
+//                            "referralId":"v-1431978351"],
+//                            
+//                        [
+//                        ["id":"5408c0bc498e56004dd0ee7b",
+//                        "name":"China Breeze",
+//                        "contact":[],
+//                        "location":[
+//                            "address":"73 Broad Street",
+//                            "lat":33.752744,
+//                            "lng":-84.392217,
+//                            "distance":21,
+//                            "cc":"US",
+//                            "city":"Atlanta",
+//                            "state":"GA",
+//                            "country":"United States",
+//                            "formattedAddress":["73 Broad Street","Atlanta, GA","United States"]],
+//                            "categories":[["id":"4bf58dd8d48988d14e941735",
+//                            "name":"American Restaurant",
+//                            "pluralName":"American Restaurants",
+//                            "shortName":"American",
+//                            "icon":["prefix":"https:\\/\\/ss3.4sqi.net\\/img\\/categories_v2\\/food\\/default_",
+//                            "suffix":".png"],
+//                            "primary":true]],
+//                            "verified":false,
+//                            "stats":["checkinsCount":2,
+//                            "usersCount":1,
+//                            "tipCount":1],
+//                            "specials":[
+//                            "count":0,
+//                            "items":[]],
+//                            "hereNow":[
+//                            "count":0,
+//                            "summary":"Nobody here",
+//                            "groups":[]],"referralId":"v-1431978351"],
+//                            ["id":"5314b2dde4b0d805d3741323"
+//                                
+//                                ]
+//                            ]
+//                            ]
+//                        ]
+//                    ]
+//            ]
+//        ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
